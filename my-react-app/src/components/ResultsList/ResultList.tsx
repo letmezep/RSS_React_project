bd8ab0becdda80c540c551f343d980aed679a01c
letmezep <letmezep@gmail.com>
2024-07-10 18:16:26 +0800

    feat: add response list from API

--- my-react-app/src/components/ResultsList/ResultList.tsx
+++ my-react-app/src/components/ResultsList/ResultList.tsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import getListFromApi from "../../services/api/getApiList"
 
 function ResultList() {
+    const [data, setData] = useState<string>('');
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await getListFromApi();
+            setData(result);
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <>
             <h3>
                 Main Field
             </h3>
+            {data && (
+                <div>
+                    {Object.entries(data).map(([key, value]) => (
+                        <div key={key}>
+                            <p><strong>{key}:</strong> {value}</p>
+                        </div>
+                    ))}
+                </div>
+            )}
         </>
     );
 }
